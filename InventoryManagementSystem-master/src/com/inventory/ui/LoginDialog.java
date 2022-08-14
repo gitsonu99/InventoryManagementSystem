76c1c00ccd638d3a2500a3dd141735d5357dd541
sonujha <jha883228@gmail.com>
2022-08-14 02:59:22 -0700

    new changes done

--- InventoryManagementSystem-master/src/com/inventory/ui/LoginDialog.java
+++ InventoryManagementSystem-master/src/com/inventory/ui/LoginDialog.java
@@ -4,8 +4,8 @@
  * and open the template in the editor.
  */
 package com.inventory.ui;
-import java.util.*;
-import java.io.*;
+import java.awt.*;
+import java.sql.*;
 import com.inventory.database.ConnectionFactory;
 import java.math.BigInteger;
 import java.security.MessageDigest;
